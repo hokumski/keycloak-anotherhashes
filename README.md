dad3570da6fe3e54fe8e7d5fa2e37604ed2bd23f
hokumski <hokum@dived.me>
2020-01-15 10:38:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# keycloak-md5
+
+Adds yet another password hash provider to Keycloak.
+
+## Install
+
+```
+curl ...
+```
+
+Restart keycloak
---
+++ src/test/java/com.keenetic.account.keycloak.md5/MD5PasswordHashProviderTest.java
@@ -0,0 +1,21 @@
+package com.keenetic.account.keycloak.md5;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class MD5PasswordHashProviderTest {
+
+  @Test
+  public void encode() {
+
+    MD5PasswordHashProvider mdd = new MD5PasswordHashProvider("md5");
+    assertEquals("df5ea29924d39c3be8785734f13169c6", mdd.encode("blabla", 0));
+    assertEquals("0dd7cb886ee8bbe82a9ea7ab2dc37efa", mdd.encode("asadd", 0));
+
+  }
+}
